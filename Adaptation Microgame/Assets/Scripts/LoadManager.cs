334ba837a82467d36cb79438ecf0a27d238fdd2d
David Wu <dawncoming3@gmail.com>
2021-10-21 23:27:08 -0400

    Game 1 Implemented
    Game 1 is now (mostly) functioning and in the main scene. I'll be implementing the octopus sprite in on the next update. Winning works, still figuring out losing mechanic? Also there are no instructions that pop up yet.

--- Adaptation Microgame/Assets/Scripts/LoadManager.cs
+++ Adaptation Microgame/Assets/Scripts/LoadManager.cs
@@ -9,12 +9,13 @@ public class LoadManager : MonoBehaviour
     private int tracker = 0;
     public GameObject gameOverScreen;
     private bool gameOver = false;
+    private GameObject game1, game2, game3, game4, game5;
 
     // Starts up the first minigame, called upon the first frame.
     void Start()
     {
-        gameOverScreen.SetActive(true);
-        Instantiate(gamePrefabs[tracker], new Vector3((float)20.9, (float)0.7, (float)16.05), Quaternion.identity);
+        gameOverScreen.SetActive(false);
+        Instantiate(gamePrefabs[0], new Vector3((float)20.9, (float)0.7, (float)16.05), Quaternion.identity);
     }
 
     // Update is called once per frame
@@ -23,33 +24,37 @@ public class LoadManager : MonoBehaviour
 
     }
 
-    void LoadMinigame()
+    public void LoadMinigame()
     {
 
     }
 
-    void Game1Win()
+    public void Game1Win()
     {
-        Destroy(gamePrefabs[tracker]);
-
-        StartCoroutine("NextGameAnimation");
-    }
-    
-    void StartGame2()
-    {
-        Instantiate(gamePrefabs[tracker], new Vector3((float)524.5164, (float)295.9212, (float)-3.378296), Quaternion.identity);
-    }
-
-    IEnumerator NextGameAnimation()
-    {
-        tracker++;
-        yield return new WaitForSeconds(1f);
+        game1 = GameObject.Find("ColorMatch(Clone)");
+        Destroy(game1);
         StartGame2();
     }
 
-    void GameOver()
+    public void Game2Win()
     {
-        Destroy(gamePrefabs[tracker]);
+        game2 = GameObject.Find("Instructions(Clone)");
+        Destroy(game2);
+        StartGame3();
+    }
+    
+    public void StartGame2()
+    {
+        Instantiate(gamePrefabs[1], new Vector3((float)524.5164, (float)295.9212, (float)-3.378296), Quaternion.identity);
+    }
+
+    public void StartGame3()
+    {
+        Instantiate(gamePrefabs[2], new Vector3((float)80.1, (float)21.1, 0), Quaternion.identity);
+    }
+
+    public void GameOver()
+    {
         gameOver = true;
         gameOverScreen.SetActive(true);
     }
